c535cf7deff6f8f5053a1df1fca386a86f0fe02d
VladyShevchenko <vladyslav.shevchenko@mev.com>
2017-01-25 23:44:06 +0200

    Task #14. Buld print file tree based of given filepath

---
+++ src/main/java/FilePrinter.java
@@ -0,0 +1,28 @@
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+public class FilePrinter {
+
+    private void buildTree(String directory) {
+
+        File rootPath = new File(directory);
+        File[] list = rootPath.listFiles();
+
+        if (list == null) return;
+
+        for (File file : list) {
+            if (file.isDirectory()) {
+                buildTree(file.getAbsolutePath());
+                System.out.println("DIRECTORY: " + file.getAbsoluteFile());
+            } else {
+                System.out.println("File: " + file.getAbsoluteFile());
+            }
+        }
+    }
+
+    @Test
+    public void testSequences() {
+        new FilePrinter().buildTree("C:\\Users\\vlady\\algorithms\\src\\");
+    }
+}
